637c7a9e485b07ffe0fe7beb8522c1427146cd1e
Joseph Halstead <josephhalstead89@gmail.com>
2018-04-17 16:02:39 -0400

    viruses

---
+++ viruses/create_jobs.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+
+if [ -z "$3" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+if [ -z "$4" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+
+if [ -z "$5" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+if [ -z "$6" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+if [ -z "$7" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+
+
+
+ACC_FILE=$1 #File containg SRR accession numbers
+BLAST_DB=$2 #Blast database
+THREADS=$3 #Number of magic blast threads to use
+OUT_DIR=$4 #Where to put the results when job in run
+JOBS_DIR=$5 #Where to put the jobs
+TEMPLATE=$6 #The grid engine submit script template
+COMMENT=$7 #Short comment for file name
+
+# clear and create jobs folder
+rm -rf $JOBS_DIR/
+mkdir $JOBS_DIR/
+
+for i in $( cat $ACC_FILE ); do
+
+  cp $TEMPLATE $JOBS_DIR/job_$i.sh
+  echo "magicblast -db $BLAST_DB -sra $i -no_unaligned -num_threads $THREADS -out $OUT_DIR/$COMMENT.$i.sam" >> $JOBS_DIR/job_$i.sh
+
+done
