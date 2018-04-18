aa0c2f4616793022c48425f24df1bc7b2fe60aad
Jozef Madzo <jmadzo@Jozefs-MacBook-Pro.local>
2018-04-18 18:03:14 -0400

    tools2

--- tools/py_fgrep.py
+++ tools/py_fgrep.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+"""
+when you try to grep the list of entries from big file >50 GB 
+grep use up too much virtual memmory and eventually crash...
+
+run py_fgerp.py column file bigFile   
+
+"""
 
 import sys
 
 try:
-    rsNumber = {line.strip() for line in open(sys.argv[1])}
+    column = sys.argv[1]-1
+    rsNumber = {line.strip() for line in open(sys.argv[2])}
 
-    with open(sys.argv[2],"r") as geno_file:
+    with open(sys.argv[column],"r") as geno_file:
         for line in geno_file:
             item = line.strip().split('\t')
             try:
@@ -14,4 +22,4 @@ try:
             except:
                 pass
 except:
-    sys.exit("no file provided\nneeds two files: file with rs_number and vcf file")
+    sys.exit("no file provided\n\nneeds two files: file with rs_number and vcf file\n\n" + __doc__)
